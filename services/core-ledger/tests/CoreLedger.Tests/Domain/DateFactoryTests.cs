a06d255bd24c0a8e89070174c147f994afe32003
Arthas <binnn852@gmail.com>
2025-09-28 18:41:17 +0300

    test(domain): add DateFactory tests with TestTimeProvider

---
+++ services/core-ledger/tests/CoreLedger.Tests/Domain/DateFactoryTests.cs
@@ -0,0 +1,23 @@
+﻿using CoreLedger.Domain.Time;
+using FluentAssertions;
+
+namespace CoreLedger.Tests.Domain;
+
+public class DateFactoryTests
+{
+    [Fact]
+    public void DateFactory_TodayPlusDays_ShouldUseProvider()
+    {
+        //Arrange
+        var fixedUtc = new DateTime(2025, 09, 28, 12, 0, 0, DateTimeKind.Utc);
+        var time = new TestTimeProvider(fixedUtc);
+
+        //Act
+        var today = DateFactory.Today(time);
+        var tomorrow = DateFactory.TodayPlusDays(time, 1);
+
+        //Assert
+        today.Should().Be(new DateOnly(2025, 09, 28));
+        tomorrow.Should().Be(new DateOnly(2025, 09, 29));
+    }
+}
---
+++ services/core-ledger/tests/CoreLedger.Tests/TestTimeProvider.cs
@@ -0,0 +1,9 @@
+﻿using CoreLedger.Domain.Time;
+
+namespace CoreLedger.Tests;
+
+public sealed class TestTimeProvider(DateTime utcNow) : ITimeProvider
+{
+    public DateTime UtcNow => utcNow;
+    public DateOnly TodayUtc => DateOnly.FromDateTime(utcNow.Date);
+}
