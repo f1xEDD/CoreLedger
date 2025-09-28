ed7e730b12fd6fb8b9d26a67003630214b9c2d35
Arthas <binnn852@gmail.com>
2025-09-28 18:08:04 +0300

    test(domain): add LedgerEntry date invariant test

---
+++ services/core-ledger/tests/CoreLedger.Tests/Domain/LedgerEntryTests.cs
@@ -0,0 +1,37 @@
+﻿using CoreLedger.Domain.Errors;
+using CoreLedger.Domain.Ledger;
+using FluentAssertions;
+
+namespace CoreLedger.Tests.Domain;
+
+public class LedgerEntryTests
+{
+    [Fact]
+    public void Ctor_ShouldThrow_WhenValueDateIsEarlierThanBookingDate()
+    {
+        // Arrange
+        var transferId = Guid.NewGuid();
+        var accountId = Guid.NewGuid();
+        var amount = new Money(10.0000m, "RUB");
+        var booking = DateOnly.FromDateTime(DateTime.UtcNow.Date);
+        var value = booking.AddDays(-1);
+
+        // Act
+        var act = () =>
+        {
+            _ = new LedgerEntry(
+                entryId: Guid.NewGuid(),
+                accountId: accountId,
+                transferId: transferId,
+                amount: amount,
+                bookingDate: booking,
+                valueDate: value,
+                direction: EntryDirection.Debit
+            );
+        };
+
+        // Assert
+        act.Should().Throw<InvariantViolation>()
+            .WithMessage("*ValueDate must be >= BookingDate*");
+    }
+}
