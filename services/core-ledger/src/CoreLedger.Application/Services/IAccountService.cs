79d3f8cd4dbde90d2754054ec06acd1e6d9ca72d
Arthas <binnn852@gmail.com>
2025-10-12 01:23:46 +0300

    feat(app): add IAccountService with Result<T>

---
+++ services/core-ledger/src/CoreLedger.Application/Services/IAccountService.cs
@@ -0,0 +1,9 @@
+﻿using CoreLedger.Application.Abstractions;
+
+namespace CoreLedger.Application.Services;
+
+public interface IAccountService
+{
+    Task<Result<Guid>> CreateAsync(string currency, CancellationToken ct);
+    Task<Result<bool>> CloseAsync(Guid accountId, CancellationToken ct);
+}
