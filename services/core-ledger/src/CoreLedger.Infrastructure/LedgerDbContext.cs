088c505742337ba72366e40a82d377e3d7ce8658
Arthas <binnn852@gmail.com>
2026-04-28 11:16:07 +0300

    feat(infra): add outbox message entity and EF configuration pt.2

--- services/core-ledger/src/CoreLedger.Infrastructure/LedgerDbContext.cs
+++ services/core-ledger/src/CoreLedger.Infrastructure/LedgerDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using CoreLedger.Domain.Accounts;
 using CoreLedger.Domain.Ledger;
 using CoreLedger.Domain.Transfers;
+using CoreLedger.Infrastructure.Outbox;
 using Microsoft.EntityFrameworkCore;
 
 namespace CoreLedger.Infrastructure;
@@ -10,6 +11,7 @@ public sealed class LedgerDbContext(DbContextOptions<LedgerDbContext> options) :
     public DbSet<Account> Accounts => Set<Account>();
     public DbSet<LedgerEntry> LedgerEntries => Set<LedgerEntry>();
     public DbSet<Transfer> Transfers => Set<Transfer>();
+    public DbSet<OutboxMessage> OutboxMessages => Set<OutboxMessage>();
 
     protected override void OnModelCreating(ModelBuilder mb)
     {
