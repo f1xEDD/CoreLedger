452a59f54d4239eea3f73e8d291bf294fbe77f71
Arthas <binnn852@gmail.com>
2026-04-28 10:53:05 +0300

    feat(infra): add migration for outbox messages

--- services/core-ledger/src/CoreLedger.Infrastructure/Migrations/20260428075227_Add_Outbox_Messages.Designer.cs
+++ services/core-ledger/src/CoreLedger.Infrastructure/Migrations/20260428075227_Add_Outbox_Messages.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using CoreLedger.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CoreLedger.Infrastructure.Migrations
 {
     [DbContext(typeof(LedgerDbContext))]
-    partial class LedgerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260428075227_Add_Outbox_Messages")]
+    partial class Add_Outbox_Messages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
