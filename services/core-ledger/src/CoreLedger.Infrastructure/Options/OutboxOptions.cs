30e393ace15f8a104f978be947d5be62031c3ef4
Arthas <binnn852@gmail.com>
2026-04-28 11:49:15 +0300

    feat(infra): add outbox dispatcher options

---
+++ services/core-ledger/src/CoreLedger.Infrastructure/Options/OutboxOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreLedger.Infrastructure.Options;
+
+public sealed class OutboxOptions
+{
+    public int BatchSize { get; init; } = 20;
+
+    public int PollingIntervalSeconds { get; init; } = 5;
+
+    public int MaxAttempts { get; init; } = 5;
+}
